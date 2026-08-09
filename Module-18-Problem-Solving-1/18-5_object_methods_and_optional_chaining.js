990cdf4201a4f593b46019e13a9e604617aae88e
Raju204116 <raju204116@gmail.com>
2026-08-09 16:28:25 +0600

    c-18-5

--- Module-18-Problem-Solving-1/18-5_object_methods_and_optional_chaining.js
+++ Module-18-Problem-Solving-1/18-5_object_methods_and_optional_chaining.js
@@ -7,10 +7,53 @@
 */
 
 
+
+const mostExpensive=(products)=>{
+
+  let productKeys = Object.keys(products);
+  // console.log(productKeys); //[ 'pen', 'book', 'bag' ]
+
+  let highest =0;
+  let expensiveProductName ="";
+  for(let key of productKeys){
+    // console.log(key); //pen, book, bag
+    // console.log(products[key]); //20,150,500
+    
+    if(highest < products[key]){ // NB: not productKeys[key] , then the output will be undefined
+      highest =products[key];
+      // console.log(highest);
+      expensiveProductName=key;
+    }
+  }
+
+  return expensiveProductName;
+}
+
+console.log(mostExpensive({ pen: 20, book: 150, bag: 500 }));
+console.log(mostExpensive({ pen: 20, book: 150, bag: 500, bat: 750, laptop: 30000, desktop: 70000 }));
+
+
+
 /*
   Problem 2: Safe Nested Access
-  Safely access deeply nested optional data using ?. and ?? without
-  throwing errors.
+  Safely access deeply nested optional data using ?. and ?? 
+  without throwing errors.
 
   Example: user?.address?.city ?? "City not found" when address is undefined
 */
+
+let student = {
+  name : "raju",
+  age :20,
+  address:{
+    village:"lohachura",
+    post:"Raninagar"
+  }
+}
+
+const safeAccess =(student)=>{
+  // return student?.address?.post ??"No Post found" ;
+  // return student?.name ??"No name found";
+  return student?.city ?? "No city found";       //NB : ?? works like else statement
+}
+console.log(safeAccess(student));
