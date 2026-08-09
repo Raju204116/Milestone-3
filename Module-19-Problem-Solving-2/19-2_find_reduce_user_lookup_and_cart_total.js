6f937a4888126d37ed813b1325ee0828a119650e
Raju204116 <raju204116@gmail.com>
2026-08-09 20:28:29 +0600

    c-19-2

--- Module-19-Problem-Solving-2/19-2_find_reduce_user_lookup_and_cart_total.js
+++ Module-19-Problem-Solving-2/19-2_find_reduce_user_lookup_and_cart_total.js
@@ -1,6 +1,7 @@
 
 /*
   Problem 1: User Lookup by ID
+
   findUserById(users, id) — find and return the single user object
   whose id matches. If no user matches,returns "User not found".
 
@@ -15,11 +16,43 @@
     { id: 2, name: "Karim" }
 */
 
+const findUserById=(users, fid) =>{
+
+  let expectedUser = users.find((eachUser) =>{
+    let {id}= eachUser;
+    // console.log(id);
+    return id ===fid ;
+  });
+  // console.log(expectedUser);
+
+  if(expectedUser){
+    return expectedUser;
+  }else
+    return "User not found."
+  
+}
+let users = [
+      { id: 1, name: "Rafi" },
+      { id: 2, name: "Karim" },
+      { id: 3, name: "Rahim" }
+    ];
+console.log(findUserById(users,2));
+console.log(findUserById(users,3));
+console.log(findUserById(users,4));
+
+
+
+
+
+
+
+
 
 
 /*
-  Problem 2: Shopping Cart Total (reduce)
-  getCartTotal(cart) — sum price * qty across every item in the cart
+  Problem 2: Shopping Cart Total 
+
+  getCartTotal(cart) — sum = price * qty across every item in the cart
   using reduce().
 
   Input:
@@ -32,5 +65,29 @@
     160   (20*3 + 50*2)
 */
 
+
+const getCartTotal=(cart)=>{
+  
+  let total =cart.reduce((accum,eachItem)=>{
+    let {price,qty}=eachItem;
+    // console.log(qty);
+    
+    accum += price* qty;
+    // console.log(accum);
+    return accum;
+    
+  },0);
+  // console.log("total=",total);
+  
+  return total;
+}
+
+let  cart = [
+      { name: "Pen", price: 20, qty: 3 },
+      { name: "Notebook", price: 50, qty: 2 },
+      // { name: "Speaker", price: 500, qty: 2 },
+      // { name: "Perfume", price: 2500, qty: 1 },
+    ];
+console.log(getCartTotal(cart));
 
 
