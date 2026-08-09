3fd17f2a288b2da3de34145623c039a36a589a32
Raju204116 <raju204116@gmail.com>
2026-08-09 20:07:47 +0600

    c-19-1

--- Module-19-Problem-Solving-2/19-1_map_filter_bulk_discount_and_passing_students.js
+++ Module-19-Problem-Solving-2/19-1_map_filter_bulk_discount_and_passing_students.js
@@ -15,6 +15,39 @@
 */
 
 
+const bulkDiscount =(prices,discountRate=0)=>{
+
+  //traditional way
+/*   let newPrices = [...prices]; //to keep the original same
+  let discount=0;
+  let priceAfterDiscount = [];
+
+  for(let element of newPrices){
+    discount = element*discountRate /100;
+    priceAfterDiscount.push(element-discount);
+  }
+  return priceAfterDiscount; */
+
+ //ES6
+ let discountedPrice=0;
+ let priceAfterDiscount = prices.map((element) =>{
+    discountedPrice = element - (element*discountRate/100);
+    return discountedPrice;
+
+ });
+
+ return priceAfterDiscount;
+
+}
+
+let prices =[500, 1000, 250];
+console.log(bulkDiscount(prices,10));
+console.log("The original:",prices);
+
+let prices2 = [500, 1000, 250, 400, 700];
+console.log(bulkDiscount(prices2,20));
+
+
 
 
 /*
@@ -36,3 +69,25 @@
       { name: "Utsho", marks: 60 },
     ]
 */
+
+const getPassingStudents=(students, threshold) =>{
+
+  let filteredStudents = students.filter((eachStudent)=>{
+    let {marks}=eachStudent;
+    // console.log(marks);
+    return marks>=threshold;
+  });
+
+  // console.log(filteredStudents);
+  return filteredStudents;
+
+}
+
+let  students = [
+      { name: "Rafi", marks: 75 },
+      { name: "Karim", marks: 40 },
+      { name: "Utsho", marks: 60 },
+      { name: "Akash", marks: 59 }
+    ];
+
+console.log(getPassingStudents(students,60));
